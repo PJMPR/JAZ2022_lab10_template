42f8e1e09556f33e3f9e0dbc7b4c45eec0e6f0a1
adam urbanowicz <adam.urbanow@gmail.com>
2022-12-14 12:48:51 +0100

    lab 10

--- westeros/westeros-web-api/src/main/java/com/westeros/webapi/WesterosWebApiApplication.java
+++ westeros/westeros-web-api/src/main/java/com/westeros/webapi/WesterosWebApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.westeroswebapi;
+package com.westeros.webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- westeros/westeros-web-api/src/test/java/com/westeros/webapi/WesterosWebApiApplicationTests.java
+++ westeros/westeros-web-api/src/test/java/com/westeros/webapi/WesterosWebApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.westeroswebapi;
+package com.westeros.webapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
