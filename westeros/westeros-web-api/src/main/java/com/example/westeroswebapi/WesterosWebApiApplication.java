6228fb2064316e348a84203c8254ddaae5d7b12d
adam urbanowicz <adam.urbanow@gmail.com>
2022-12-14 12:29:37 +0100

    lab 10

---
+++ westeros/westeros-web-api/src/main/java/com/example/westeroswebapi/WesterosWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.westeroswebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesterosWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WesterosWebApiApplication.class, args);
+    }
+
+}
